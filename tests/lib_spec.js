0f312f57e49e47e6037e5b9a25b492f7fab6478d
Yann Lerjen <yann.lerjen@gmail.com>
2016-02-03 21:45:01 +0100

    add a spec file to tests the dummy function

---
+++ tests/lib_spec.js
@@ -0,0 +1,15 @@
+describe("The Hello World function", function() {
+    
+  it("should say hello to the passed name", function() {
+    var name = 'Yann';
+    var result = helloWorld(name);
+    var expected = 'Hello Yann';
+    expect(result).toEqual(expected);
+  });
+  
+  it("should say hello to 'world' if no name is passed", function() {
+    var result = helloWorld();
+    var expected = 'Hello World';
+    expect(result).toEqual(expected);
+  });
+});
